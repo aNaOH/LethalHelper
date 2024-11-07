79b99f0c9764a2cd5b25056431ed5447ad80af4d
Abel <abehsosa2004@gmail.com>
2024-11-07 15:54:56 +0100

    Updated structure and added stuff

--- AbelLethalHelper/ScrapList.cs
+++ AbelLethalHelper/ScrapList.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AbelMuak.Libs.LethalHelper
 {
 
     [Serializable]
-    public struct ScrapInfo
+    public class ScrapInfo
     {
         public Item ItemDef;
         public int Rarity;
@@ -19,10 +20,25 @@ namespace AbelMuak.Libs.LethalHelper
         }
     }
 
+    public class ScrapInfoJSON
+    {
+        public string ItemDef;
+        public int Rarity;
+        public int LevelType;
+
+        public ScrapInfoJSON(string ItemDef, int Rarity, int LevelType = -1)
+        {
+            this.ItemDef = ItemDef;
+            this.Rarity = Rarity;
+            this.LevelType = LevelType;
+        }
+    }
+
     [CreateAssetMenu(fileName = "ScrapList", menuName = "Abel/Scrap list", order = 0)]
+    [Serializable]
     public class ScrapList : ScriptableObject
     {
-        public ScrapInfo[] ScrapInfos;
+        public List<ScrapInfo> ScrapInfos;
     }
 
 }
---
+++ LethalHelperPlugin/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace AbelMuak.Libs.LethalHelper
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        public const string PLUGIN_GUID = "com.abelmuak.libs.lethalthings";
+        public const string PLUGIN_NAME = "Lethal Helper";
+        public const string PLUGIN_VERSION = "1.0.3";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        }
+    }
+}
